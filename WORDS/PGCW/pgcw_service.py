6d859c6f2e0e65a6e66bd2558410e5f19b95cf0d
Mostafa Kavand <mk323mk323mk32@gmail.com>
2023-03-01 21:23:29 +0330

    Update pgcw_service.py
    sorry. The task of this file was to do the work based on the order of the characters entered by the user. We confused this with the task of another file. Now the previous function is put back

--- WORDS/PGCW/pgcw_service.py
+++ WORDS/PGCW/pgcw_service.py
@@ -18,14 +18,14 @@
 
 import itertools as it
 
-def permutations_functions(*, file_path, start_range, end_range, word_list):
+def combinations_functions(*, file_path, start_range, end_range, word_list):
     try:
         # open the pass-list file after the loops to avoid repetitive action => reduce cpu usage
         with open(file_path, 'w') as file:
             # loop over range of lengths
             for i in range(start_range, end_range + 1):
                 # use generator expression to build all combinations of length i
-                prob_pass_list = (''.join(combination) for combination in it.permutations(word_list, i))
+                prob_pass_list = (''.join(combination) for combination in it.combinations(word_list, i))
                 # write to file in chunks of 1000 lines at a time
                 chunk_size = 1000
                 for chunk in iter(lambda: list(it.islice(prob_pass_list,chunk_size)), []):
