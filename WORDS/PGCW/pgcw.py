f83acd522222e149a679f5eafc245a69d01851dc
Mohammadreza-Rokni <110774079+Mohammadreza-Rokni@users.noreply.github.com>
2023-03-01 21:34:59 +0330

    last fix pgcw

--- WORDS/PGCW/pgcw.py
+++ WORDS/PGCW/pgcw.py
@@ -15,7 +15,7 @@ import pgcw_service as pgcwms
 def start_calculation():
     word_list = list(cas.get_words_from_usr())
     start, end = cas.get_pass_range_from_user()
-    pgcwms.permutations_functions(file_path='WORDS\PGCW\pgcw_pass_list.txt',
+    pgcwms.combinations_functions(file_path='WORDS\PGCW\pgcw_pass_list.txt',
                                   start_range=start, end_range=end, word_list=word_list)
     
 if __name__ == "__main__" :
